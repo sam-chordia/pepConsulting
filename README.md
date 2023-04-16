b59636f9633dcb0a5577f20ed8e8de5d82e9e0a0
Sambhav Chordia <sambhavchordia03@gmail.com>
2023-04-16 15:56:02 -0400

    wrote README and added code

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# pepConsulting
+
+
+<!-- ABOUT THE PROJECT -->
+# Predicition Verifiaction
+
+This project is meant to serve the goal of verifying the findings of model predictions regarding PEP against real world data.
+
+
+## Built With
+
+This project was built entirely with R.
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+### Prerequisites
+
+In order to run this script your would need to have R and R studio downloaded onto your system.
+
+### Installation
+
+To run the program simply execute the line below in Rstudio once in the correct base directory.
+   ```sh
+   source("predVerification.R", encoding = "UTF-8")
+   ```
+
+
+Once you run the file, it should create a folder with 2 files within it.
+
+    predictionValues
+    ├── answers_pred_sub.xlsx       # Contains the predictions from the fully trained GBM model
+    ├── answers_pred.xlsx           # Contains the predictions from the model on TRT subsets
---
+++ predVerification.R
@@ -0,0 +1,52 @@
+# predVerification.R
+# -----------------------------------------------------------------------------
+# Author:             Sambhav Chordia
+# Date last modified: April 2023
+#
+# R script to predict values for the PEPRISC data set using the GBM model
+
+
+# Load required packages for reading and writing from xlsx files
+library(readxl)
+library(writexl)
+
+# Set file paths
+excel_path <- "PEPRISC Val Data PRELIM.xlsx"
+excel_pred_path <- "predictionValues/answers_pred.xlsx"
+excel_pred_sub_path <- "predictionValues/answers_pred_sub.xlsx"
+
+# copies over all the data models from the data folder
+
+# Model on full dataset
+fit <- readRDS("data/gbm_model.rds")
+# Model on trt subsets
+fit_sub <- readRDS("data/gbm_model_trt.rds") 
+# Training dataset (unnormalized)
+train <- readRDS("data/train_new.rds")
+# Imputed and normalized training dataset
+train_impute <- readRDS("data/train_impute.rds")
+# Variable labels
+var_names <- readRDS("data/var_names.rds")
+# LIME explainer for variable importance
+lime_explainer <- readRDS("data/lime_explainer.rds")
+
+
+# Read in data from Excel sheet
+print("Reading in data from Excel sheet...")
+data <- read_excel(excel_path, .name_repair = "unique")
+
+
+# Predict values
+print("Using prediction models to predict values...")
+pred_fit <- predict(fit, newdata = data, type = "prob")
+pred_fit_sub <- predict(fit_sub, newdata = data, type = "prob")
+# pred_train <- predict(train, newdata = data, type = "prob")
+# pred_train_impute <- predict(train_impute, newdata = data, type = "prob")
+# pred_var_names <- predict(var_names, newdata = data, type = "prob")
+# pred_line <- predict(lime_explainer, newdata = data, type = "response")
+
+
+# Append predictions to Excel sheet
+print("Writing predictions to Excel sheet...")
+write_xlsx(pred_fit, excel_pred_path, col_names = TRUE)
+write_xlsx(pred_fit_sub, excel_pred_sub_path, col_names = TRUE)
